32854a886e08eb18042fcaff41aaa05e9936905c
josesanchez080880 <josesanchez080880@gmail.com>
2024-01-02 19:10:35 -0600

    commit init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import Header from '@/components/Hearder'
 import '@/styles/globals.css'
 
+import { Poppins } from 'next/font/google'
+
+const inter = Poppins({ subsets: ['latin'], weight: '400' })
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <main className={`${inter.className} min-h-screen max-w-screen-2x1 mx-auto px-4 bg-background text-accent`}>
+    <Header />
+    <Component {...pageProps} />
+    </main>
+  </>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'text': '#000000',
+        'background': '#FFFFFF',
+        'primary': '#5dc1b9',
+        'secondary': '#00a2d3',
+        'accent': '#232F2E',
+      }
     },
   },
   plugins: [],
